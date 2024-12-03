eefafd0ff38b80da0e8c7661a56294ff48372be2
VASEEM <92212839+vaseem9084@users.noreply.github.com>
2024-12-03 16:05:27 +0530

    added script.js file

---
+++ Script.js
@@ -0,0 +1,22 @@
+function myMenuFunction(){
+    var menuBth = document.getElementById("myNaveMenu");
+
+    if (menuBth.className === "nav-menu") {
+        menuBth.className += "responsive";
+    }
+    else{
+        menuBth.className = "nav-menu";
+    }
+}
+
+// dark mode
+
+const body = document.querySelector("body"),
+    toggleSwitch = document.getElementById("toggle-switch");
+
+toggleSwitch.addEventListener("click", ()=> {
+    body.classList.toggle("dark");
+});
+
+
+
